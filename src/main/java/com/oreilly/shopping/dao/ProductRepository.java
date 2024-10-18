a8d9527143983d96d4c5c29dfd8373de5e813d8d
mkhmmd17 <maxmelhi95@gmail.com>
2024-10-18 13:12:24 -0400

    Create a Product Entity

---
+++ src/main/java/com/oreilly/shopping/dao/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.oreilly.shopping.dao;
+
+import com.oreilly.shopping.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> { //takes Generic Entity type and Primarly Key
+
+    
+}
