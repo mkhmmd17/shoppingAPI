9cac5117f469c5a04a6712abadadefbbccc9869f
mkhmmd17 <maxmelhi95@gmail.com>
2024-10-18 14:12:51 -0400

    Added ComandLineRunner

--- src/main/java/com/oreilly/shopping/ShoppingApplication.java
+++ src/main/java/com/oreilly/shopping/ShoppingApplication.java
@@ -1,7 +1,14 @@
 package com.oreilly.shopping;
 
+import com.oreilly.shopping.dao.ProductRepository;
+import com.oreilly.shopping.entities.Product;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 @SpringBootApplication
 public class ShoppingApplication {
@@ -10,4 +17,19 @@ public class ShoppingApplication {
         SpringApplication.run(ShoppingApplication.class, args);
     }
 
+
+    @Bean
+    public CommandLineRunner initalizeDatabase(ProductRepository productRepository) {
+        return args -> {
+            if(productRepository.count() == 0) {} {
+                productRepository.saveAll(
+                        List.of(
+                                new Product("TV tray", BigDecimal.valueOf(4.95)),
+                                new Product("Toaster", BigDecimal.valueOf(19.95)),
+                                new Product("Sofa", BigDecimal.valueOf(249.95))
+                        )
+                ).forEach(System.out::println);
+            }
+        };
+    }
 }
